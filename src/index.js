c07ab5dfc346c7cc51fd708951b0aec7a9a9b384
argaitt <argaitt@gmail.com>
2018-09-15 14:19:18 +0300

    solution/argaitt

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var nextIndex, count = 0;
+    for (var i = 0; i < preferences.length; i++){
+ 
+         if ((preferences[i] - 1 == i) || (preferences[i] < 1)) continue;
+         else nextIndex = preferences[i] - 1;
+ 
+         if ((preferences[nextIndex] - 1 == nextIndex) || (preferences[nextIndex] < 1)) continue
+         else nextIndex = preferences[nextIndex] - 1;
+ 
+         if (preferences[nextIndex] - 1 != i) continue
+         else count++;
+ 
+    } 
+    return count / 3;
 };
